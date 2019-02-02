198da2ea5a7b954da2115bd8f202e5a7d3d7c638
davidlefebvre19 <davidlfbvr19@gmail.com>
2019-02-02 14:14:57 +0100

    library

---
+++ Velocity1/Velocity1.ino
@@ -0,0 +1,9 @@
+
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SoftwareSerial.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME680.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
