1d5e21c0a2bf09f078b60c72053cad95eb88a616
jean-christophe bauduin <bauduinjc@gmail.com>
2019-02-03 13:24:54 +0100

    de nombreuses erreurs a en plus finir: - les variables ne sont pas declares - les fonctions ne sont pas declares convenablement , rappel , pour creer une fonction qui ne retourne pas de valeurs => void mafonction() le void est obligaoitre - les variables changenet de noms a tout bout de champ=> genre le pc va comprendre que TempBME est egale a tempBMP (genre wtf) - des fonctions qui n existent pas sont appeles - la lecture du bme n est pas bonne, VOUS N AVEZ PAS LU LA DOCUMENTATION pour preuve vous appelez pas les fonctions qui doivent etre appeles

--- Velocity1/Velocity1.ino
+++ Velocity1/Velocity1.ino
@@ -12,6 +12,36 @@
 #define SEALEVELPRESSURE_HPA (1013.25)
 Adafruit_BME680 bme; // I2C
 
+/* il faut declarer ses variable */
+float TempBME,PresBME,AltBME,Humidity,Gas;
+void getBME() {
+  // il faut lire la doc les enfants ... :
+  if (! bme.performReading()) { // je lance la lecture
+    Serial.println("Failed to perform reading :("); // si ca pete j affiche et je retourne rien poru arreter
+    return;
+  }
+  // vous avez oublie de declarer toutes les variables
+  TempBME = bme.temperature;
+  PresBME = bme.pressure / 100;
+  AltBME = bme.readAltitude(SEALEVELPRESSURE_HPA);
+  Humidity = bme.humidity;
+  Gas = bme.gas_resistance / 1000.0;
+  //  saveData((String)F("BMP: ") + TempBMP + F(";") + PresBMP + F(";") + AltBMP+F(";")+Humidity+F(";")+Gas);
+  // c est quoi cette connerie , vous n avez meme pas mis les memes noms de variables
+  saveData((String)F("BMP: ") + TempBME + F(";") + PresBME + F(";") + AltBME+F(";")+Humidity+F(";")+Gas);
+}
+
+
+void saveData(String dump) {
+
+  //Send((String)dump); // vous n avez pas declarer send ...
+   Serial.println(dump);
+  File dataFile = SD.open("ATT.TXT", FILE_WRITE);
+
+  dataFile.println(dump);
+  dataFile.close();
+
+}
 void setup (){
     // Set up oversampling and filter initialization
   bme.setTemperatureOversampling(BME680_OS_8X);
@@ -23,24 +53,4 @@ void setup (){
 
 void loop () {
   getBME();
-}
-
-getBME(); {
-  TempBME = bme.temperature;
-  PresBME = bme.pressure / 100;
-  AltBME = bme.readAltitude(SEALEVELPRESSURE_HPA);
-  Humidity = bme.humidity;
-  Gas = bme.gas_resistance / 1000.0;
-  saveData((String)F("BMP: ") + TempBMP + F(";") + PresBMP + F(";") + AltBMP+F(";")+Humidity+F(";")+Gas);
-}
-
-void saveData(String dump) {
-
-  Send((String)dump);
-
-  File dataFile = SD.open("ATT.TXT", FILE_WRITE);
-
-  dataFile.println(dump);
-  dataFile.close();
-
 }
