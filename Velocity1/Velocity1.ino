b0126955177c773fb9fd57e771e65325d3864909
davidlefebvre19 <davidlfbvr19@gmail.com>
2019-02-03 13:05:11 +0100

    bme normal test _ data is save in sd card _ no print ln

--- Velocity1/Velocity1.ino
+++ Velocity1/Velocity1.ino
@@ -7,3 +7,40 @@
 #include <Adafruit_BME680.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
+
+//BME config
+#define SEALEVELPRESSURE_HPA (1013.25)
+Adafruit_BME680 bme; // I2C
+
+void setup (){
+    // Set up oversampling and filter initialization
+  bme.setTemperatureOversampling(BME680_OS_8X);
+  bme.setHumidityOversampling(BME680_OS_2X);
+  bme.setPressureOversampling(BME680_OS_4X);
+  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+  bme.setGasHeater(320, 150); // 320*C for 150 ms
+}
+
+void loop () {
+  getBME();
+}
+
+getBME(); {
+  TempBME = bme.temperature;
+  PresBME = bme.pressure / 100;
+  AltBME = bme.readAltitude(SEALEVELPRESSURE_HPA);
+  Humidity = bme.humidity;
+  Gas = bme.gas_resistance / 1000.0;
+  saveData((String)F("BMP: ") + TempBMP + F(";") + PresBMP + F(";") + AltBMP+F(";")+Humidity+F(";")+Gas);
+}
+
+void saveData(String dump) {
+
+  Send((String)dump);
+
+  File dataFile = SD.open("ATT.TXT", FILE_WRITE);
+
+  dataFile.println(dump);
+  dataFile.close();
+
+}
