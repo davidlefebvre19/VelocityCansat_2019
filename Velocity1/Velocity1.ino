8eca21a62f7352dc81986d9a566b6c3c88013d7c
David Lefebvre <davidlfbvr19@gmail.com>
2019-02-05 00:10:43 +0100

    implemantation du bno en mode 4g reussis, donnee non save dans le sd et utilisation des delay --> pas de uint ou jsp quoi, a faire

--- Velocity1/Velocity1.ino
+++ Velocity1/Velocity1.ino
@@ -12,8 +12,12 @@
 #define SEALEVELPRESSURE_HPA (1013.25)
 Adafruit_BME680 bme; // I2C
 
-/* il faut declarer ses variable */
+//BNO config
+#define BNO055_SAMPLERATE_DELAY_MS (100)
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
 float TempBME,PresBME,AltBME,Humidity,Gas;
+
+
 void getBME() {
   // il faut lire la doc les enfants ... :
   if (!bme.performReading()) { // je lance la lecture
@@ -27,25 +31,60 @@ void getBME() {
   Humidity = bme.humidity;
   Gas = bme.gas_resistance / 1000.0;
   //  saveData((String)F("BMP: ") + TempBMP + F(";") + PresBMP + F(";") + AltBMP+F(";")+Humidity+F(";")+Gas);
-  // c est quoi cette connerie , vous n avez meme pas mis les memes noms de variables
   saveData((String)F("BMP: ") + TempBME + F(";") + PresBME + F(";") + AltBME+F(";")+Humidity+F(";")+Gas);
+}
+
+void getBNO() {
+  //nv event pr bno
+  sensors_event_t event;
+  bno.getEvent(&event);
+
+    /* Display the floating point data */
+  Serial.print("X: ");
+  Serial.print(event.orientation.x, 4);
+  Serial.print("\tY: ");
+  Serial.print(event.orientation.y, 4);
+  Serial.print("\tZ: ");
+  Serial.print(event.orientation.z, 4);
+
+  /* Optional: Display calibration status */
+
+  /* Optional: Display sensor status (debug only) */
+  //displaySensorStatus();
+
+  /* New line for the next sample */
+  Serial.println("");
+
+  /* Wait the specified delay before requesting nex data */
+  delay(BNO055_SAMPLERATE_DELAY_MS);
 }
 
 
 void saveData(String dump) {
-
-  //Send((String)dump); // vous n avez pas declarer send ...
    Serial.println(dump);
   File dataFile = SD.open("ATT.TXT", FILE_WRITE);
 
   dataFile.println(dump);
   dataFile.close();
-
 }
+
+
 void setup (){
+    Serial.begin(9600);
+    
+    //bme initialisation
     if (!bme.begin()) {
     Serial.println("Could not find a valid BME680 sensor, check wiring!");
     while (1);
+  }
+
+    //bno initialisation
+    if(!bno.begin()){
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+
+    delay(1000);
   }
 
     // Set up oversampling and filter initialization
@@ -58,4 +97,5 @@ void setup (){
 
 void loop () {
   getBME();
+  getBNO();
 }
