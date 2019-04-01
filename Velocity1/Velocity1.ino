f6a7ce44e4869285c56486aef57e89a2366d50f6
David Lefebvre <davidlfbvr19@gmail.com>
2019-04-01 11:52:31 +0200

    plein de bug

--- Velocity1/Velocity1.ino
+++ Velocity1/Velocity1.ino
@@ -6,6 +6,7 @@
 //#include <Adafruit_Sensor.h>
 #include "Adafruit_BME680.h"
 #include <Adafruit_BNO055.h>
+#include <Adafruit_GPS.h>
 //#include <utility/imumaths.h>
 
 
@@ -15,6 +16,39 @@ SoftwareSerial xbee(19,18);
 //BME config
 #define SEALEVELPRESSURE_HPA (1013.25)
 Adafruit_BME680 bme; // I2C
+
+//GPS config
+SoftwareSerial mySerial(3, 2);
+#define GPSECHO  true
+Adafruit_GPS GPS(&mySerial);
+boolean usingInterrupt = false;
+void useInterrupt(boolean);
+
+// Interrupt is called once a millisecond, looks for any new GPS data, and stores it
+SIGNAL(TIMER0_COMPA_vect) {
+  char c = GPS.read();
+  // if you want to debug, this is a good time to do it!
+#ifdef UDR0
+  if (GPSECHO)
+    if (c) UDR0 = c;  
+    // writing direct to UDR0 is much much faster than Serial.print 
+    // but only one character can be written at a time. 
+#endif
+}
+
+void useInterrupt(boolean v) {
+  if (v) {
+    // Timer0 is already used for millis() - we'll just interrupt somewhere
+    // in the middle and call the "Compare A" function above
+    OCR0A = 0xAF;
+    TIMSK0 |= _BV(OCIE0A);
+    usingInterrupt = true;
+  } else {
+    // do not call the interrupt function COMPA anymore
+    TIMSK0 &= ~_BV(OCIE0A);
+    usingInterrupt = false;
+  }
+}
 
 //BNO config
 #define BNO055_SAMPLERATE_DELAY_MS (100)
@@ -101,6 +135,63 @@ void getPitot() {
   saveData((String)F("PITOT: ") + veloc);
 }
 
+void getGPS () {
+    // in case you are not using the interrupt above, you'll
+  // need to 'hand query' the GPS, not suggested :(
+  if (! usingInterrupt) {
+    // read data from the GPS in the 'main loop'
+    char c = GPS.read();
+    // if you want to debug, this is a good time to do it!
+    if (GPSECHO)
+      if (c) Serial.print(c);
+  }
+  
+  // if a sentence is received, we can check the checksum, parse it...
+  if (GPS.newNMEAreceived()) {
+    // a tricky thing here is if we print the NMEA sentence, or data
+    // we end up not listening and catching other sentences! 
+    // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
+    //Serial.println(GPS.lastNMEA());   // this also sets the newNMEAreceived() flag to false
+  
+    if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
+      return;  // we can fail to parse a sentence in which case we should just wait for another
+  }
+
+  // if millis() or timer wraps around, we'll just reset it
+  if (timer > millis())  timer = millis();
+
+  // approximately every 2 seconds or so, print out the current stats
+  if (millis() - timer > 2000) { 
+    timer = millis(); // reset the timer
+    
+    Serial.print("\nTime: ");
+    Serial.print(GPS.hour, DEC); Serial.print(':');
+    Serial.print(GPS.minute, DEC); Serial.print(':');
+    Serial.print(GPS.seconds, DEC); Serial.print('.');
+    Serial.println(GPS.milliseconds);
+    Serial.print("Date: ");
+    Serial.print(GPS.day, DEC); Serial.print('/');
+    Serial.print(GPS.month, DEC); Serial.print("/20");
+    Serial.println(GPS.year, DEC);
+    Serial.print("Fix: "); Serial.print((int)GPS.fix);
+    Serial.print(" quality: "); Serial.println((int)GPS.fixquality); 
+    if (GPS.fix) {
+      Serial.print("Location: ");
+      Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
+      Serial.print(", "); 
+      Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
+      Serial.print("Location (in degrees, works with Google Maps): ");
+      Serial.print(GPS.latitudeDegrees, 4);
+      Serial.print(", "); 
+      Serial.println(GPS.longitudeDegrees, 4);
+      
+      Serial.print("Speed (knots): "); Serial.println(GPS.speed);
+      Serial.print("Angle: "); Serial.println(GPS.angle);
+      Serial.print("Altitude: "); Serial.println(GPS.altitude);
+      Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
+    }
+  }
+}
 
 void saveData(String dump) {
   File dataFile = SD.open(F("ATT.TXT"), FILE_WRITE);
@@ -116,6 +207,12 @@ void saveData(String dump) {
 void setup (){
     Serial.begin(9600);
     xbee.begin(9600);
+
+    //GPS initialisation
+    if(!GPS.begin()) {
+    while (1);
+  }
+    
     //bme initialisation
     if (!bme.begin()) {
     while (1);
@@ -131,6 +228,14 @@ void setup (){
   }
   
   offset /= offset_size;
+
+    //GPS initialisation part 2 
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
+  GPS.sendCommand(PGCMD_ANTENNA);
+  
+  useInterrupt(true);
+
   
     // Set up oversampling and filter initialization
   bme.setTemperatureOversampling(BME680_OS_8X);
@@ -140,6 +245,8 @@ void setup (){
   bme.setGasHeater(320, 150); // 320*C for 150 ms
 }
 
+
+uint32_t timer = millis();
 void loop () {
   unsigned long currentMillis = millis();
   
@@ -152,6 +259,7 @@ void loop () {
   getBNO();
   getNano();
   getPitot();
+  getGPS();
   }
 
 }
