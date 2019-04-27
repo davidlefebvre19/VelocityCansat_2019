e6bcd8bff4db2b4d295021ec468d132819b427dc
David Lefebvre <davidlfbvr19@gmail.com>
2019-04-27 18:32:09 +0200

    GPS w errors

--- Velocity1/Velocity1.ino
+++ Velocity1/Velocity1.ino
@@ -11,6 +11,15 @@
 #include <RHReliableDatagram.h>
 #include <RH_RF95.h>
 #include <SPI.h>
+#include <Adafruit_GPS.h>
+
+//GPS 
+HardwareSerial mySerial = Serial2;
+Adafruit_GPS GPS(&Serial2);
+#define GPSECHO  false
+boolean usingInterrupt = false;
+void useInterrupt(boolean);
+
 
 //LoRa config
 #define RFM95_CS 4
@@ -62,75 +71,6 @@ boolean buzzerActif= false;
 //
 float TempBME,PresBME,AltBME,Humidity,Gas,acc_x,acc_z,acc_y;
 
-void getBME() {
-  if (!bme.performReading()) {
-    return;
-  }
-  // vous avez oublie de declarer toutes les variables
-  TempBME = bme.temperature;
-  PresBME = bme.pressure / 100;
-  AltBME = bme.readAltitude(SEALEVELPRESSURE_HPA);
-  Humidity = bme.humidity;
-  Gas = bme.gas_resistance / 1000.0;
-  saveData((String)F("BMP: ") + TempBME + F(";") + PresBME + F(";") + AltBME+F(";")+Humidity+F(";")+Gas);
-  //saveData((String)F("BMP: ") + TempBME + conca+ PresBME + conca+ AltBME+conca+Humidity+conca+Gas);
-}
-
-int16_t packetnum = 0;  // packet counter, we increment per xmission
-
-void getBNO() {
-  //nv event pr bno
-  sensors_event_t event;
-  bno.getEvent(&event);
-
-  //imu::Quaternion quat = bno.getQuat();
-  imu::Vector<3> magnet = bno.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
-  imu::Vector<3> gyrosc = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
-  //imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
-  imu::Vector<3> acc = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
-  //imu::Vector<3> lacc = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
-  //imu::Vector<3> grav = bno.getVector(Adafruit_BNO055::VECTOR_GRAVITY);
-  
-  double AccX = acc.x();
-  double AccY = acc.y();
-  double AccZ = acc.z();
-  saveData((String)"BNOA: " + AccX + ";" + AccY + ";" + AccZ);
-
-  //delay(BNO055_SAMPLERATE_DELAY_MS);
-}
-
-String dat;
-
-void getPitot() {
-  float adc_avg = 0; float veloc = 0.0;
-  
-// average a few ADC readings for stability
-  for (int ii=0;ii<veloc_mean_size;ii++){
-    adc_avg+= analogRead(A0)-offset;
-  }
-  adc_avg/=veloc_mean_size;
-  
-  // make sure if the ADC reads below 512, then we equate it to a negative velocity
-  if (adc_avg>512-zero_span and adc_avg<512+zero_span){
-  } else{
-    if (adc_avg<512){
-      veloc = -sqrt((-10000.0*((adc_avg/1023.0)-0.5))/rho);
-    } else{
-      veloc = sqrt((10000.0*((adc_avg/1023.0)-0.5))/rho);
-    }
-  }
-  saveData((String)F("PITOT: ") + veloc);
-}
-
-void getBuzzer() {
-  //buzzer et impact
-  fsrReading = analogRead(fsrPin);
-  saveData((String)F("FSR: ") + fsrReading);
-  if (buzzerActif ==true){
-    tone(buzzerPin,1000,90000);
-  }
-}
-
 void saveData(String dump) {
   startSD();
   File dataFile = SD.open(F("ATT.txt"), FILE_WRITE);
@@ -175,6 +115,131 @@ void saveData(String dump) {
 startSD();
 }
 
+void getBME() {
+  if (!bme.performReading()) {
+    return;
+  }
+  // vous avez oublie de declarer toutes les variables
+  TempBME = bme.temperature;
+  PresBME = bme.pressure / 100;
+  AltBME = bme.readAltitude(SEALEVELPRESSURE_HPA);
+  Humidity = bme.humidity;
+  Gas = bme.gas_resistance / 1000.0;
+  saveData((String)F("BMP: ") + TempBME + F(";") + PresBME + F(";") + AltBME+F(";")+Humidity+F(";")+Gas);
+  //saveData((String)F("BMP: ") + TempBME + conca+ PresBME + conca+ AltBME+conca+Humidity+conca+Gas);
+}
+
+int16_t packetnum = 0;  // packet counter, we increment per xmission
+
+void getBNO() {
+  //nv event pr bno
+  sensors_event_t event;
+  bno.getEvent(&event);
+
+  //imu::Quaternion quat = bno.getQuat();
+  imu::Vector<3> magnet = bno.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
+  imu::Vector<3> gyrosc = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+  //imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+  imu::Vector<3> acc = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  //imu::Vector<3> lacc = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+  //imu::Vector<3> grav = bno.getVector(Adafruit_BNO055::VECTOR_GRAVITY);
+  
+  double AccX = acc.x();
+  double AccY = acc.y();
+  double AccZ = acc.z();
+  saveData((String)"BNOA: " + AccX + ";" + AccY + ";" + AccZ);
+
+  //delay(BNO055_SAMPLERATE_DELAY_MS);
+}
+
+//GPS
+SIGNAL(TIMER0_COMPA_vect) {
+  char c = GPS.read();
+  // if you want to debug, this is a good time to do it!
+#ifdef UDR0
+  if (GPSECHO)
+    if (c) UDR0 = c;  
+    // writing direct to UDR0 is much much faster than Serial.print 
+    // but only one character can be written at a time. 
+#endif
+}
+
+void useInterrupt(boolean v) {
+  if (v) {
+    // Timer0 is already used for millis() - we'll just interrupt somewhere
+    // in the middle and call the "Compare A" function above
+    OCR0A = 0xAF;
+    TIMSK0 |= _BV(OCIE0A);
+    usingInterrupt = true;
+  } else {
+    // do not call the interrupt function COMPA anymore
+    TIMSK0 &= ~_BV(OCIE0A);
+    usingInterrupt = false;
+  }
+}
+
+uint32_t timer = millis();
+char c;
+void getGPS() {
+ if (GPS.newNMEAreceived()) {
+
+    if (!GPS.parse(GPS.lastNMEA()))
+      return;
+  }
+
+  if (timer > millis())  timer = millis();
+
+  delay(10);
+
+  if (millis() - timer > 200) {
+    timer = millis();
+
+    String GPS_data = "";
+    if (GPS.fix==1) {
+      String lat = String(GPS.latitudeDegrees,4);
+      String lon = String(GPS.longitudeDegrees,4);
+      GPS_data += lat+";"+lon;
+      GPS_data += ";" + String(GPS.altitude,1);
+      GPS_data += ";";
+    } else{
+      GPS_data += "0;0;0;";
+    }
+}
+
+String dat;
+
+void getPitot() {
+  float adc_avg = 0; float veloc = 0.0;
+  
+// average a few ADC readings for stability
+  for (int ii=0;ii<veloc_mean_size;ii++){
+    adc_avg+= analogRead(A0)-offset;
+  }
+  adc_avg/=veloc_mean_size;
+  
+  // make sure if the ADC reads below 512, then we equate it to a negative velocity
+  if (adc_avg>512-zero_span and adc_avg<512+zero_span){
+  } else{
+    if (adc_avg<512){
+      veloc = -sqrt((-10000.0*((adc_avg/1023.0)-0.5))/rho);
+    } else{
+      veloc = sqrt((10000.0*((adc_avg/1023.0)-0.5))/rho);
+    }
+  }
+  saveData((String)F("PITOT: ") + veloc);
+}
+
+void getBuzzer() {
+  //buzzer et impact
+  fsrReading = analogRead(fsrPin);
+  saveData((String)F("FSR: ") + fsrReading);
+  if (buzzerActif ==true){
+    tone(buzzerPin,1000,90000);
+  }
+}
+
+
+
 void getLora() {
 
 }
@@ -193,6 +258,17 @@ void setup (){
     Serial.begin(9600);
     xbee.begin(9600);
 
+    //GPS
+    GPS.begin(9600);
+    GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+    GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); 
+    GPS.sendCommand(PGCMD_ANTENNA);
+    useInterrupt(true);
+
+  delay(1000);
+
+  mySerial.println(PMTK_Q_RELEASE);
+    
     //LoRa
     pinMode(53,OUTPUT);
     pinMode(RFM95_RST, OUTPUT);
@@ -259,6 +335,7 @@ void loop () {
   getBME();
   getBNO();
   getPitot();
+  getGPS();
   getBuzzer();
   getLora();
   }  
